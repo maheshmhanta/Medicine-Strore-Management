08cd4106a96f934eb7df633616241329447ef367
Akash pawar <ap28198@gmail.com>
2023-05-27 12:43:55 +0530

    completed_project review it

--- login_window.py
+++ login_window.py
@@ -3,20 +3,16 @@ from tkinter import *
 from PIL import ImageTk, Image
 import mysql.connector
 import tkinter.messagebox
-
-def submitact():
-
-	user = Username.get()
-	passw = password.get()
-
 from tkinter import ttk
 
-
+def submitact():
+	user = Username.get()
+	passw = password.get()
 # Function for login button on the login screen
 def submit():
     global username, password, actual
     username = Username.get()
-    password = str(password.get())
+    password =str(password.get())
     query = "SELECT Password from dbusers where Username = %s"
     values = (username,)
     mycursor.execute(query, values)
@@ -27,6 +23,80 @@ def submit():
     except:
         tkinter.messagebox.showinfo("Error", "Invalid Username", parent = root)
 
+
+def Add():
+    item = e1.get()
+    quantity = (e2.get())
+    price = (e3.get())
+    
+
+    mysqldb=mysql.connector.connect(host="localhost",user="root",password="akash",database="medicine",auth_plugin="mysql_native_password")
+    mycursor=mysqldb.cursor()
+
+    try:
+       sql = "INSERT INTO  store VALUES (%s, %s, %s)"
+       val = (item,quantity,price)
+       mycursor.execute(sql, val)
+       mysqldb.commit()
+       lastid = mycursor.lastrowid
+       tkinter.messagebox.showinfo("information", "Medicine inserted successfully...")
+       e1.delete(0, END)
+       e2.delete(0, END)
+       e3.delete(0, END)
+       e1.focus_set()
+    except Exception as e:
+       print(e)
+       mysqldb.rollback()
+       mysqldb.close()
+
+def delete():
+    item = e1.get()
+
+    mysqldb=mysql.connector.connect(host="localhost",user="root",password="akash",database="medicine",auth_plugin="mysql_native_password")
+    mycursor=mysqldb.cursor()
+
+    try:
+       sql = "delete from store where item = %s"
+       val = (item,)
+       mycursor.execute(sql, val)
+       mysqldb.commit()
+       tkinter.messagebox.showinfo("information", "Record Deleteeeee successfully...")
+
+       e1.delete(0, END)
+       e2.delete(0, END)
+       e3.delete(0, END)
+       e1.focus_set()
+
+    except Exception as e:
+
+       print(e)
+       mysqldb.rollback()
+       mysqldb.close()
+
+def update():
+    item = e1.get()
+    quantity = e2.get()
+    price = e3.get()
+    mysqldb=mysql.connector.connect(host="localhost",user="root",password="akash",database="medicine",auth_plugin="mysql_native_password")
+    mycursor=mysqldb.cursor()
+
+    try:
+       sql = "Update  store set quantity= %s,price= %s where item = %s"
+       val = (quantity,price,item)
+       mycursor.execute(sql, val)
+       mysqldb.commit()
+       tkinter.messagebox.showinfo("information", "Record Updateddddd successfully...")
+
+       e1.delete(0, END)
+       e2.delete(0, END)
+       e3.delete(0, END)
+       e1.focus_set()
+
+    except Exception as e:
+
+       print(e)
+       mysqldb.rollback()
+       mysqldb.close()
 
 def mainFun():
     global root
@@ -55,24 +125,43 @@ def main():
     e3=Entry(root)
     e3.place(x=140,y=70)
 
-    Button(root,text="Add",height=2,width=9).place(x=30,y=130)
-    Button(root,text="Update",height=2,width=9).place(x=140,y=130)
-    Button(root,text="Delete",height=2,width=9).place(x=250,y=130)
+    Button(root,text="Add",height=2,width=9,command=Add).place(x=30,y=130)
+    Button(root,text="Update",height=2,width=9,command=update).place(x=140,y=130)
+    Button(root,text="Delete",height=2,width=9,command=delete).place(x=250,y=130)
+    Button(root,text="Refresh",height=2,width=9,command=refresh).place(x=350,y=130)
 
     cols = ('ITEM', 'Quantity', 'Price')
+    global listBox
     listBox = ttk.Treeview(root, columns=cols, show='headings' )
     
     for col in cols:
         listBox.heading(col, text=col)
         listBox.grid(row=1, column=0, columnspan=2)
         listBox.place(x=10, y=200)
-    '''
+
     show()
-    listBox.bind('<Double-Button-1>',GetValue)
-    '''
+    
+    
     root.mainloop()
 
 
+def refresh():
+    #Clear the treeview list items
+    for item in listBox.get_children():
+        listBox.delete(item)
+    show()
+
+
+def show():
+        mysqldb = mysql.connector.connect(host="localhost", user="root", password="akash",database="medicine",auth_plugin="mysql_native_password")
+        mycursor = mysqldb.cursor()
+        mycursor.execute("SELECT * FROM store")
+        records = mycursor.fetchall()
+        print(records)
+
+        for i, (stname, course,fee) in enumerate(records, start=1):
+            listBox.insert("", "end", values=(stname, course, fee))
+            mysqldb.close()
 
 
 
